dcdda50fcadcf0c5fc6e7fbf2ad5793b0ec41a54
Renato Romao <renatolsromao@gmail.com>
2019-09-08 23:00:00 -0300

    ref: Change cep_dag and cep_queries to use staging

--- airflow/plugins/helpers/cep_queries.py
+++ airflow/plugins/helpers/cep_queries.py
@@ -1,7 +1,7 @@
 cep_queries = {
 
     'create_cep_table': """
-        create table if not exists cep (
+        create table if not exists staging.cep (
             cep int,
             logradouro text,
             bairro text,
@@ -11,7 +11,7 @@ cep_queries = {
     """,
 
     'create_cities_table': """
-        create table if not exists cities (
+        create table if not exists staging.cities (
             cidade_id int,
             cidade_nome text,
             estado_id int
@@ -19,7 +19,7 @@ cep_queries = {
     """,
 
     'create_states_table': """
-        create table if not exists states (
+        create table if not exists staging.states (
             estado_id int,
             estado_nome text,
             estado_sigla char(2)
